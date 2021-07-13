671285a6bf2d8aee86c1eecd7517f571cadcda47
Jacky Tang <jackytang74@gmail.com>
2021-07-13 14:39:54 -0700

    completed 03 SELECT from Nobel and minor edit to question 12 for 02 SELECT from World

--- 02 SELECT from World.sql
+++ 02 SELECT from World.sql
@@ -64,6 +64,8 @@
 -- 12. The capital of Sweden is Stockholm. Both words start with the letter 'S'.
   -- Show the name and the capital where the first letters of each match.
   -- Don't include countries where the name and the capital are the same word.
+    -- You can use the function LEFT to isolate the first character.
+    -- You can use <> as the NOT EQUALS operator.
     SELECT name, capital
         FROM world
         WHERE name <> capital AND LEFT(name, 1) = LEFT(capital, 1);
