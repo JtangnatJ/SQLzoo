4c46d1fa11926592622163bd50299c65a9b1e92b
Jacky Tang <jackytang74@gmail.com>
2021-07-14 16:18:47 -0700

    04 SELECT within SELECT harder problems 9 and 10

--- 04 SELECT within SELECT.sql
+++ 04 SELECT within SELECT.sql
@@ -29,7 +29,7 @@
 
 -- 5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
   -- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
-    SELECT name, CONCAT(CAST(ROUND(100*population / (SELECT population FROM world WHERE name = 'Germany'), 0) as int), %)
+    SELECT name, CONCAT(CAST(ROUND(100*population / (SELECT population FROM world WHERE name = 'Germany'), 0) as int), %) AS percentage
         FROM world WHERE continent = 'Europe';
 
 -- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
@@ -50,4 +50,20 @@
             SELECT name FROM world y
                 WHERE x.continent = y.continent);
 
+-- 9. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents.
+  -- Show name, continent and population.
+    SELECT name, continent, population FROM world x
+        WHERE 25e6 >= ALL (
+            SELECT population FROM world y
+                WHERE y.continent = x.continent
+                    AND population > 0
+        ); 
 
+-- 10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+    SELECT name, continent FROM world x
+        WHERE (x.population / 3) > ALL (
+            SELECT population FROM world y
+                WHERE y.continent = x.continent
+                    AND population > 0
+                    AND y.name != x.name
+        );
