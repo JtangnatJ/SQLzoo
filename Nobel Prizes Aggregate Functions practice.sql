4205d601c4bb5e67d7da92874f76bcd1b73fe501
Jacky Tang <jackytang74@gmail.com>
2021-07-15 15:24:52 -0700

    nobel aggregate functions practice #3

--- Nobel Prizes Aggregate Functions practice.sql
+++ Nobel Prizes Aggregate Functions practice.sql
@@ -3,3 +3,7 @@
 
 -- 2. List each subject - just once
     SELECT DISTINCT subject FROM nobel;
+
+-- 3. Show the total number of prizes awarded for Physics.
+    SELECT COUNT(subject) FROM nobel
+        WHERE subject = 'Physics';
